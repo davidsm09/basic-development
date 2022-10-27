4097bc67c9fe8a5feb77d4fc020068f9c5113b49
davidcol <mezad55@gmail.com>
2022-10-26 22:04:23 -0500

    segundo commit

--- src/main.js
+++ src/main.js
@@ -1,24 +1,29 @@
 function aleatorio(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min)
-  }
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
 
-  function eleccion(jugada) {
-    let resultado = ""
-    if(jugada == 1) {
-        resultado = "Piedra 🪨"
-    } else if (jugada == 2) {
-        resultado = "Papel 📃"
-    } else if (jugada == 3) {
-        resultado = "Tijera ✂️"
-    } else {
-        resultado = "MAL ELEGIDO"
-    }
-    return resultado
+function eleccion(jugada) {
+  let resultado = ""
+  if(jugada == 1) {
+    resultado = "Piedra 🪨"
+  } else if (jugada == 2) {
+    resultado = "Papel 📃"
+  } else if (jugada == 3) {
+    resultado = "Tijera ✂️"
+  } else {
+    resultado = "MAL ELEGIDO"
   }
+  return resultado
+}
 
-  // 1 es piedra, 2 es papel, 3 es tijera
-  let jugador = 0
-  let pc = aleatorio(1,3)
+// 1 es piedra, 2 es papel, 3 es tijera
+let jugador = 0
+let pc = 0
+let triunfos = 0
+let perdidas = 0
+
+while (triunfos < 3 && perdidas < 3) {
+  pc = aleatorio(1,3)
   jugador = prompt("Elige: 1 para piedra, 2 para papel, 3 para tijera")
   // alert("Elegiste " + jugador)
 
@@ -26,14 +31,16 @@ function aleatorio(min, max) {
   alert("Tú eliges " + eleccion(jugador))
   
   // COMBATE
-  if (pc == jugador) {
+  // COMBATE
+  if(pc == jugador) {
     alert("EMPATE")
-  } else if (jugador == 1 && pc == 3) {
-    alert("GANASTE")
-  } else if (jugador == 2 && pc == 1) {
-    alert("GANASTE")
-  } else if (jugador == 3 && pc == 2) {
-    alert("GANASTE")
-  } else {
+    } else if((jugador == 1 && pc == 3) || (jugador == 2 && pc ==1) || (jugador ==3& pc ==2)) {
+    triunfos++
+      alert("GANASTE")
+    } else {
+      perdidas++
     alert("PERDISTE")
-  }
+    }
+}
+
+alert("Ganaste " + triunfos + " veces. Perdiste " + perdidas + " veces.")
