fdaa289579b940d021559d4e68db11f9c987c176
davidcol <mezad55@gmail.com>
2022-11-01 22:04:54 -0500

    funcionalidad

--- Mokepon/juego.js
+++ Mokepon/juego.js
@@ -1,6 +1,18 @@
+let ataqueJugador
+let ataqueEnemigo
+
 function iniciarJuego() {
     let botonMascotaJugador = document.getElementById('boton-mascota')
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+
+    let botonFuego = document.getElementById('boton-fuego')
+    botonFuego.addEventListener('click, ataqueFuego')
+    let botonAgua = document.getElementById('boton-Agua')
+    botonFuego.addEventListener('click, ataqueAgua')
+    let botonTierra =document.getElementById('boton-Tierra')
+    botonFuego.addEventListener('click, ataqueTierra')
+    
+
 }
 
 function seleccionarMascotaJugador() {
@@ -18,6 +30,52 @@ function seleccionarMascotaJugador() {
     } else {
         spanMascotaJugador.innerHTML = 'Elige una Mascota!'
     }
+
+    seleccionarMascotaEnemigo()
+}
+
+function seleccionarMascotaEnemigo() {
+    let ataqueAleatorio = aleatorio(1, 3)
+    let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
+
+    if (ataqueAleatorio == 1){
+        spanMascotaEnemigo.innerHTML = "Hipodogue"
+    } else if (ataqueAleatorio == 2){
+        spanMascotaEnemigo.innerHTML = "Capipepo"
+    } else {
+        spanMascotaEnemigo.innerHTML = "Ratihueya"
+    }
+
+}
+
+function ataqueFuego () {
+    ataqueJugador = 'Fuego'
+    ataqueAleatorioEnemigo()
+}
+
+function ataqueAgua () {
+    ataqueJugador = 'Agua'
+    ataqueAleatorioEnemigo()
+
+function ataqueTierra () {
+    ataqueJugador = 'Tierra'
+    ataqueAleatorioEnemigo()
+}
+
+function ataqueAleatorioEnemigo(){
+    let ataqueAleatorio = aleatorio(1, 3);
+    
+    if (ataqueAleatorio == 1) {
+        ataqueEnemigo = 'Fuego'
+    } else if (ataqueAleatorio == 2){
+        ataqueEnemigo = 'Agua'
+    } else {
+        ataqueEnemigo = 'Tierra'
+    }
+}
+
+function aleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 window.addEventListener('load', iniciarJuego)
